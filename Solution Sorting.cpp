756a8963fdcef8362d6dab3966a0f0f5c3ccb286
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-06-27 11:15:00 +0200

    Create Solution Sorting.cpp

---
+++ Solution Sorting.cpp
@@ -0,0 +1,33 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+    vector<int> minAvailableDuration(vector<vector<int>>& slots_firstPerson, vector<vector<int>>& slots_secondPerson, int duration) {
+
+        auto smallerStartingTimeOnTop = [](const auto& x, const auto& y) {return x[0] - y[0];};
+        sort(slots_firstPerson.begin(), slots_firstPerson.end(), smallerStartingTimeOnTop);
+        sort(slots_secondPerson.begin(), slots_secondPerson.end(), smallerStartingTimeOnTop);
+
+        int index_first = 0;
+        int index_second = 0;
+
+        while (index_first < slots_firstPerson.size() && index_second < slots_secondPerson.size()) {
+
+            int maxStart = max(slots_firstPerson[index_first][0], slots_secondPerson[index_second][0]);
+            int minEnd = min(slots_firstPerson[index_first][1], slots_secondPerson[index_second][1]);
+            if (minEnd - maxStart >= duration) {
+                return vector<int>{maxStart, maxStart + duration};
+            }
+
+            if (slots_firstPerson[index_first][1] < slots_secondPerson[index_second][1]) {
+                ++index_first;
+            } else {
+                ++index_second;
+            }
+        }
+        return vector<int>();
+    }
+};
