1ce54444b9eec69f582ccd3444bdd7c46d602c34
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-06-27 11:21:49 +0200

    Update Solution Sorting.cpp

--- Solution Sorting.cpp
+++ Solution Sorting.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots_firstPerson, vector<vector<int>>& slots_secondPerson, int duration) {
 
-        auto smallerStartingTimeOnTop = [](const auto& x, const auto& y) {return x[0] - y[0];};
+        auto smallerStartingTimeOnTop = [](const auto& x, const auto& y) {return x[0] < y[0];};
         sort(slots_firstPerson.begin(), slots_firstPerson.end(), smallerStartingTimeOnTop);
         sort(slots_secondPerson.begin(), slots_secondPerson.end(), smallerStartingTimeOnTop);
 
